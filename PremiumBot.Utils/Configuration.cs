18434d1faacd0cfe87cc7419510619f9003d9c89
Nuparu55 <nuparu19988991@gmail.com>
2022-06-05 11:59:59 +0300

    RM ignored files

--- PremiumBot.Utils/Configuration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PremiumBot.Utils
-{
-    public static class Configuration
-    {
-        //Insert your values
-        public static readonly string BOT_TOKEN = "";
-        public static readonly string DB_CONNECTION_STRING = "";
-    }
-}
